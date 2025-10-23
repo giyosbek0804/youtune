1974bf938a239d07962ceccdf419eda17f910924
Giyosbek <giyosbekkodirberganov@gmail.com>
2025-10-23 15:01:49 +0500

    starting core with api

--- src/pages/main/homePage.jsx
+++ src/pages/main/homePage.jsx
@@ -1,47 +1,68 @@
 import { Link, Outlet } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const API_KEY = import.meta.env.VITE_YOUTUBE_API_KEY;
+
 
 function HomePage() {
-  const [videos, setVideos] = useState([]);
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState("react")
+  useEffect(() => {
+    async function getData() {
+      try {
+        const res = await axios.get(
+          `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&type=video&maxResults=6&key=${API_KEY}`
+        );
+        console.log(res);
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    getData();
+  }, []);
+  return (
+    <>
+      <h1>ok</h1>
+    </>
+  );
+}
+export default HomePage;
 
-  const API_KEY = import.meta.env.VITE_YOUTUBE_API_KEY;
 
+function Home1Page() {
   async function searchVideos() {
     const response = await fetch(
       `https://www.googleapis.com/youtube/v3/search?part=snippet&q=${query}&type=video&maxResults=6&key=${API_KEY}`
     );
     const data = await response.json();
-      setVideos(data.items);
-      console.log(data );
-      
+    setVideos(data.items);
+    console.log(data);
   }
- return (
-   <div className="p-4">
-     <h1>YouTube Search</h1>
+  return (
+    <div className="p-4">
+      <h1>YouTube Search</h1>
 
-     <input
-       value={query}
-       onChange={(e) => setQuery(e.target.value)}
-       placeholder="Search videos..."
-       className="border px-2 py-1 rounded"
-     />
-     <button
-       onClick={searchVideos}
-       className="ml-2 px-3 py-1 bg-blue-500 text-white rounded"
-     >
-       Search
-     </button>
+      <input
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        placeholder="Search videos..."
+        className="border px-2 py-1 rounded"
+      />
+      <button
+        onClick={searchVideos}
+        className="ml-2 px-3 py-1 bg-blue-500 text-white rounded"
+      >
+        Search
+      </button>
 
-     <div className="grid grid-cols-3 gap-4 mt-4">
-       {videos.map((v) => (
-         <div key={v.id.videoId}>
-           <img src={v.snippet.thumbnails.medium.url} alt={v.snippet.title} />
-           <h3 className="text-sm mt-2">{v.snippet.title}</h3>
-         </div>
-       ))}
-     </div>
-   </div>
- );
+      <div className="grid grid-cols-3 gap-4 mt-4">
+        {videos.map((v) => (
+          <div key={v.id.videoId}>
+            <img src={v.snippet.thumbnails.medium.url} alt={v.snippet.title} />
+            <h3 className="text-sm mt-2">{v.snippet.title}</h3>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
-export default HomePage;
